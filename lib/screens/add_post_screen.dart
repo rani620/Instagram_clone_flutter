44b79794c352680ed988b2ff16d77c55e468d377
rani <rkcrj100@gmail.com>
2023-01-14 11:49:18 +0530

    add post section  added

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -1,5 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:instagram_flutter/providers/user_provider.dart';
 import 'package:instagram_flutter/utils/color.dart';
+import 'package:instagram_flutter/utils/utils.dart';
+import 'package:provider/provider.dart';
 
 class AddPostScreen extends StatefulWidget {
   const AddPostScreen({super.key});
@@ -9,81 +15,118 @@ class AddPostScreen extends StatefulWidget {
 }
 
 class _AddPostScreenState extends State<AddPostScreen> {
+  Uint8List? _file;
 
-// .............
+  _selectImage(BuildContext) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return SimpleDialog(
+            title: const Text('Creat a post'),
+            children: [
+              SimpleDialogOption(
+                padding: const EdgeInsets.all(20),
+                child: const Text('Take a photo'),
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                  Uint8List file = await pickImage(ImageSource.camera);
+                  setState(() {
+                    _file = file;
+                  });
+                },
+              ),
+              SimpleDialogOption(
+                padding: const EdgeInsets.all(20),
+                child: const Text('Choose from gallery'),
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                  Uint8List file = await pickImage(
+                    ImageSource.gallery,
+                  );
+                  setState(() {
+                    _file = file;
+                  });
+                },
+              )
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    // return Center(
-    //   child: IconButton(
-    //     icon: const Icon(
-    //       Icons.upload
-    //     ),
-    //     onPressed: () {
+    // final User user = Provider.of<UserProvider>(context).getUser;
 
-    //     },
-    //     ),
-    // );
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: mobileBackgroundColor,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {},
-        ),
-        title: const Text('post to'),
-        centerTitle: false,
-        actions: [
-          TextButton(
-            onPressed: () {},
-            child: const Text(
-              'Post',
-              style: TextStyle(
-                color: Colors.blueAccent,
-                fontWeight: FontWeight.bold,
-                fontSize: 16,
-              ),
+    // final User user = Provider.of<UserProvider>(context).getUser;
+    final User user = Provider.of<UserProvider>(context).getUser;
+     
+    return _file == null
+        ? Center(
+            child: IconButton(
+              icon: const Icon(Icons.upload),
+              onPressed: () => _selectImage(context),
             ),
           )
-        ],
-      ),
-      body: Column(children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CircleAvatar(
-              backgroundImage: AssetImage('assets/rani.png'),
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.45,
-              child: TextField(
-                decoration: const InputDecoration(
-                  hintText: 'write a caption....',
-                  border: InputBorder.none,
-                  hintMaxLines: 8,
-                ),
+        : Scaffold(
+            appBar: AppBar(
+              backgroundColor: mobileBackgroundColor,
+              leading: IconButton(
+                icon: const Icon(Icons.arrow_back),
+                onPressed: () {},
               ),
+              title: const Text('post to'),
+              centerTitle: false,
+              actions: [
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Post',
+                    style: TextStyle(
+                      color: Colors.blueAccent,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                )
+              ],
             ),
-            SizedBox(
-              height: 45,
-              width: 45,
-              child: AspectRatio(
-                aspectRatio: 487 / 451,
-                child: Container(
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                    image: AssetImage('assets/rani.png'),
-                    fit: BoxFit.fill,
-                    alignment: FractionalOffset.topCenter,
-                  )),
-                ),
-              ),
-            ),
-            const Divider(),
-          ],
-        )
-      ]),
-    );
+            body: Column(children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    backgroundImage: AssetImage('assets/rani.png'),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.45,
+                    child: TextField(
+                      decoration: const InputDecoration(
+                        hintText: 'write a caption....',
+                        border: InputBorder.none,
+                        hintMaxLines: 8,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 45,
+                    width: 45,
+                    child: AspectRatio(
+                      aspectRatio: 487 / 451,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                          image: AssetImage('assets/rani.png'),
+                          fit: BoxFit.fill,
+                          alignment: FractionalOffset.topCenter,
+                        )),
+                      ),
+                    ),
+                  ),
+                  const Divider(),
+                ],
+              )
+            ]),
+          );
   }
 }
